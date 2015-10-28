basecamp/trix

commit f06b1fca4562be92c0631bcd676319beceb60ade

Check for dropped files first to avoid error reading dataTransfer in IE

src/trix/controllers/input_controller.coffee
@@ -163,7 +163,10 @@ class Trix.InputController extends Trix.BasicObject
       point = [event.clientX, event.clientY]
       @responder?.setLocationRangeFromPoint(point)
 
-      if @draggedRange
+      if files = event.dataTransfer?.files
+        @attachFiles(files)
+
+      else if @draggedRange
         @delegate?.inputControllerWillMoveText()
         @responder?.moveTextFromRange(@draggedRange)
         @draggedRange = null
@@ -173,9 +176,6 @@ class Trix.InputController extends Trix.BasicObject
         document = Trix.Document.fromJSONString(documentJSON)
         @responder?.insertDocument(document)
         @requestRender()
-
-      else if files = event.dataTransfer.files
-        @attachFiles(event.dataTransfer.files)
 
       @draggedRange = null
       @draggingPoint = null

commit af1dd1f538f73fe493fd5fb1786d942cbdfbc63a

Fix dragging selections
I quietly broke this with f06b1fca4562be92c0631bcd676319beceb60ade

src/trix/controllers/input_controller.coffee
@@ -160,10 +160,12 @@ class Trix.InputController extends Trix.BasicObject
 
     drop: (event) ->
       event.preventDefault()
+      files = event.dataTransfer?.files
+
       point = [event.clientX, event.clientY]
       @responder?.setLocationRangeFromPoint(point)
 
-      if files = event.dataTransfer?.files
+      if files?.length
         @attachFiles(files)
 
       else if @draggedRange

test/src/test_helpers/input_helpers.coffee
@@ -149,9 +149,14 @@ for code, name of Trix.InputController.keyNames
 
 @dragToCoordinates = (coordinates, callback) ->
   element = document.activeElement
+
+  dropData = dataTransfer: files: []
+  dropData[key] = value for key, value of coordinates
+
   triggerEvent(element, "mousemove")
   triggerEvent(element, "dragstart")
-  triggerEvent(element, "drop", coordinates)
+  triggerEvent(element, "drop", dropData)
+
   defer(callback)
 
 @mouseDownOnElementAndMove = (element, distance, callback) ->