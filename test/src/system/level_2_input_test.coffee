basecamp/trix

commit c518d03506a9689c8c27eaac4776a8a47f244dd0

Guard against inserting empty/undefined strings
Fixes:

Uncaught TypeError: Cannot read property 'replace' of undefined
  at normalizeNewlines (trix.js:1492)
  at new StringPiece (trix.js:7140)
  at Function.Trix.Text.Text.textForStringWithAttributes (trix.js:7521)
  at Composition.Trix.Composition.Composition.insertString (trix.js:9787)

src/trix/controllers/level_2_input_controller.coffee
@@ -227,15 +227,11 @@ class Trix.Level2InputController extends Trix.InputController
 
     insertCompositionText: ->
       @composing = true
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(@event.data)
+      @insertString(@event.data)
 
     insertFromComposition: ->
       @composing = false
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(@event.data)
+      @insertString(@event.data)
 
     insertFromDrop: ->
       if range = @deleteByDragRange
@@ -289,16 +285,12 @@ class Trix.Level2InputController extends Trix.InputController
           @delegate?.inputControllerDidPaste(paste)
 
     insertFromYank: ->
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(@event.data)
+      @insertString(@event.data)
 
     # insertHorizontalRule: ->
 
     insertLineBreak: ->
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString("\n")
+      @insertString("\n")
 
     insertLink: ->
       @activateAttributeIfSupported("href", @event.data)
@@ -312,24 +304,24 @@ class Trix.Level2InputController extends Trix.InputController
         @responder?.insertLineBreak()
 
     insertReplacementText: ->
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(@event.dataTransfer.getData("text/plain"), updatePosition: false)
+      @insertString(@event.dataTransfer.getData("text/plain"), updatePosition: false)
 
     insertText: ->
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(@event.data)
+      @insertString(@event.data)
 
     insertTranspose: ->
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(@event.data)
+      @insertString(@event.data)
 
     insertUnorderedList: ->
       @toggleAttributeIfSupported("bullet")
 
   # Responder helpers
+
+  insertString: (string, options) ->
+    if string
+      @delegate?.inputControllerWillPerformTyping()
+      @withTargetDOMRange ->
+        @responder?.insertString(string, options)
 
   toggleAttributeIfSupported: (attributeName) ->
     if attributeName in Trix.getAllAttributeNames()

commit d9143c0d607da9bfb743dc8acfdbaeef558f61fb

Fix deleting the last character in a composed word on Android
Regression introduced in https://github.com/basecamp/trix/commit/c518d03506a9689c8c27eaac4776a8a47f244dd0

src/trix/controllers/level_2_input_controller.coffee
@@ -336,11 +336,10 @@ class Trix.Level2InputController extends Trix.InputController
 
   # Responder helpers
 
-  insertString: (string, options) ->
-    if string
-      @delegate?.inputControllerWillPerformTyping()
-      @withTargetDOMRange ->
-        @responder?.insertString(string, options)
+  insertString: (string = "", options) ->
+    @delegate?.inputControllerWillPerformTyping()
+    @withTargetDOMRange ->
+      @responder?.insertString(string, options)
 
   toggleAttributeIfSupported: (attributeName) ->
     if attributeName in Trix.getAllAttributeNames()

test/src/system/level_2_input_test.coffee
@@ -1,4 +1,4 @@
-{assert, after, clickToolbarButton, defer, insertString, insertNode, isToolbarButtonActive, selectAll, test, testIf, testGroup, triggerEvent, triggerInputEvent, typeCharacters} = Trix.TestHelpers
+{assert, after, clickToolbarButton, defer, insertString, insertNode, isToolbarButtonActive, selectAll, selectNode, test, testIf, testGroup, triggerEvent, triggerInputEvent, typeCharacters} = Trix.TestHelpers
 
 test = ->
   testIf(Trix.config.input.getLevel() is 2, arguments...)
@@ -123,6 +123,17 @@ testGroup "Level 2 Input", testOptions, ->
       document.activeElement.dispatchEvent(event)
       requestAnimationFrame ->
         expectDocument "one\n"
+
+  # https://input-inspector.now.sh/profiles/yZlsrfG93QMzp2oyr0BE
+  test "deleting the last character in a composed word on Android", (expectDocument) ->
+    insertString("c")
+    element = getEditorElement()
+    textNode = element.firstChild.lastChild
+    selectNode textNode, ->
+      triggerInputEvent(element, "beforeinput", inputType: "insertCompositionText", data: "")
+      triggerEvent(element, "compositionend", data: "")
+      requestAnimationFrame ->
+        expectDocument "\n"
 
   test "pasting a file", (expectDocument) ->
     createFile (file) ->