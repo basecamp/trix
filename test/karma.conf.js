760a86331666dcab974e8d2728cdae2a7e0fa77d
Javan Makhmali <javan@javan.us>
2020-03-09 15:05:34 -0700

    CI: Freshen browser test matrix

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,25 +33,25 @@ if (process.env.CI) {
       base: "SauceLabs",
       browserName: "chrome",
       platform: "Windows 10",
-      version: "76"
+      version: "latest"
     },
     sl_firefox_latest: {
       base: "SauceLabs",
       browserName: "firefox",
       platform: "Windows 10",
-      version: "68"
+      version: "latest"
     },
     sl_safari_previous: {
       base: "SauceLabs",
       browserName: "safari",
-      platform: "macOS 10.12",
-      version: "11.0"
+      platform: "macOS 10.13",
+      version: "latest-1"
     },
     sl_safari_latest: {
       base: "SauceLabs",
       browserName: "safari",
       platform: "macOS 10.13",
-      version: "12.1"
+      version: "latest"
     },
     sl_edge_previous: {
       base: "SauceLabs",
