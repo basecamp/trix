a316247073bbfdc9d0a32c658e8b55be5cdabb1f
Alberto Fernandez-Capel <alberto@hey.com>
2022-01-14 16:10:45 +0000

    Export models under the Trix global namespace
    For compatibility with existing apps.

--- src/trix/global.js
+++ src/trix/global.js
@@ -1,10 +1,13 @@
 import { version } from "../../package.json"
 import config from "trix/config"
+import models from "trix/models"
 
 const Trix = {
   VERSION: version,
   config,
 }
+
+Object.assign(Trix, models)
 
 window.Trix = Trix
 
---
+++ src/trix/models.js
@@ -0,0 +1,41 @@
+import Attachment from "./models/attachment"
+import AttachmentManager from "./models/attachment_manager"
+import AttachmentPiece from "./models/attachment_piece"
+import Block from "./models/block"
+import Composition from "./models/composition"
+import Cocument from "./models/document"
+import Editor from "./models/editor"
+import HtmlParser from "./models/html_parser"
+import HtmlSanitizer from "./models/html_sanitizer"
+import LineBreakInsertion from "./models/line_break_insertion"
+import LocationMapper from "./models/location_mapper"
+import ManagedAttachment from "./models/managed_attachment"
+import Piece from "./models/piece"
+import PointMapper from "./models/point_mapper"
+import SelectionManager from "./models/selection_manager"
+import SplittableList from "./models/splittable_list"
+import StringPiece from "./models/string_piece"
+import Text from "./models/text"
+import UndoManager from "./models/undo_manager"
+
+export default {
+  Attachment,
+  AttachmentManager,
+  AttachmentPiece,
+  Block,
+  Composition,
+  Cocument,
+  Editor,
+  HtmlParser,
+  HtmlSanitizer,
+  LineBreakInsertion,
+  LocationMapper,
+  ManagedAttachment,
+  Piece,
+  PointMapper,
+  SelectionManager,
+  SplittableList,
+  StringPiece,
+  Text,
+  UndoManager,
+}
