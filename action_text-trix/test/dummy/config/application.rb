b9efc5b0ed2c283bc48754a4342dd1259a9e0c67
Sean Doyle <sean.p.doyle24@gmail.com>
2025-09-26 10:17:58 -0400

    Introduce `trix/actiontext` module
    In an effort to absorb responsibilities from `@rails/actiontext` (namely
    the [app/javascript/actiontext/index.js][] and
    [app/javascript/actiontext/attachment_upload.js][] files), this commit
    introduces a new `trix/actiontext` file that the build process will
    output to be consumable from the `action_text-trix` engine's asset
    directory.
    
    The `trix/actiontext` module will depend on two in-browser dependencies:
    
    * `trix` expected to be imported separately
    * `@rails/activestorage` expected to available for import. Since there
      is a direct dependency on `@rails/activestorage` through the
      `@rails/acitontext` package, the dependency will dependably (😉) be
      present.
    
    In support of this change, this commit also expands the system test
    coverage introduced by [#1258][] to also account for
    `direct-upload:`-prefixed events dispatched during file uploads.
    
    [app/javascript/actiontext/index.js]: https://github.com/rails/rails/blob/v8.0.3/actiontext/app/javascript/actiontext/index.js
    [app/javascript/actiontext/attachment_upload.js]: https://github.com/rails/rails/blob/v8.0.3/actiontext/app/javascript/actiontext/attachment_upload.js
    [#1258]: https://github.com/basecamp/trix/pull/1258

--- action_text-trix/test/dummy/config/application.rb
+++ action_text-trix/test/dummy/config/application.rb
@@ -26,6 +26,6 @@ module Dummy
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_storage.variant_processor = :disabled
+    config.active_storage.variant_processor = :vips
   end
 end
