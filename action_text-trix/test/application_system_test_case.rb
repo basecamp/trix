c55b6d8e835fa08d4682e48910b92ddaff9a7328
Jorge Manrubia <jorge@37signals.com>
2026-08-21 12:40:29 +0200

    Give Chrome longer to boot in system tests
    Ferrum's 10 second default process_timeout is too tight on loaded CI
    runners, where Chrome intermittently fails to publish its websocket
    URL in time and the whole matrix cell errors before running.

--- action_text-trix/test/application_system_test_case.rb
+++ action_text-trix/test/application_system_test_case.rb
@@ -5,7 +5,9 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :cuprite, using: :chrome, options: {
     js_errors: true,
-    headless: ENV["HEADLESS"] != "0"
+    headless: ENV["HEADLESS"] != "0",
+    # Ferrum's 10s default is too tight for Chrome to boot on loaded CI runners
+    process_timeout: 60
   }
 end
 
