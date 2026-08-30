771f42123c9ef3a834e42cf21fe6b5208770ae03
Oskar Eichler <oskar@songstats.com>
2026-08-30 04:22:38 +0200

    Store only key-value pairs in Hash arrays

--- action_text-trix/app/assets/javascripts/trix.js
+++ action_text-trix/app/assets/javascripts/trix.js
@@ -1397,7 +1397,7 @@ $\
         const result = [];
         for (const key in this.values) {
           const value = this.values[key];
-          result.push(result.push(key, value));
+          result.push(key, value);
         }
         this.array = result.slice(0);
       }
